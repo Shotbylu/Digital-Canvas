f896865d2e5b201ea8975deea7a505dedd4b9541
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-11-23 02:53:54 +0200

    Update index.ts

--- src/components/campaigns/index.ts
+++ src/components/campaigns/index.ts
@@ -1,2 +1,3 @@
-export { default as CampaignCard } from './campaign-card';
-export * from './video-modal';
+export { default as CampaignFilters } from './CampaignFilters';
+export { default as CampaignCard } from './CampaignCard';
+export { default as VideoModal } from './VideoModal';
